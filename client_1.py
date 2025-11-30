18ef72126a89776c1ee44be6d5d7acd8fc23bfdb
sneesna <snaasnee@gmail.com>
2025-11-29 20:22:47 -0500

    fixed overwrite again
    accidentally pasted the server.py lololol

--- client_1.py
+++ client_1.py
@@ -1,186 +1,131 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
 # Author : Ayesha S. Dina
 
 import os
 import socket
-import threading
-from time import time
 
+
+# IP = "192.168.1.101" #"localhost"
 IP = "localhost"
 PORT = 4450
 ADDR = (IP,PORT)
-SIZE = 1024
+SIZE = 1024 ## byte .. buffer size
 FORMAT = "utf-8"
-SERVER_PATH = "server"
+SERVER_DATA_PATH = "server_data"
 
-COUNTER = 1
-
-### to handle the clients
-def handle_client (conn,addr):
-
-    print(f"[NEW CONNECTION] {addr} connected.")
-    conn.send("OK@Welcome to the server".encode(FORMAT))
+def main():
     
-    # list to track uploaded files
-    uploaded = []
-    
-    # list to track subdirectories (for later)
-
-    while True:
-        data =  conn.recv(SIZE).decode(FORMAT)
-        data = data.split("@")
-        cmd = data[0]
-       
-        send_data = "OK@"
-
-        if cmd == "LOGOUT":
+    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    client.connect(ADDR)
+    while True:  ### multiple communications
+        data = client.recv(SIZE).decode(FORMAT)
+        cmd, msg = data.split("@")
+        if cmd == "OK":
+            print(f"{msg}")
+        elif cmd == "DISCONNECTED":
+            print(f"{msg}")
             break
-
-        elif cmd == "TASK": 
-            send_data += "LOGOUT from the server.\n"
-            send_data += "UPLOAD a file to the server.\n"
-            send_data += "DOWNLOAD a file from the server.\n"
-            send_data += "DELETE a file from the server.\n"
-
-            conn.send(send_data.encode(FORMAT))
-
-        elif cmd == "UPLOAD":
-            # send to client_1 that you're ready
-            send_data += "Ready to receive the file.\n"
-            conn.send(send_data.encode(FORMAT))
-            
-            # get filename
-            filename = conn.recv(SIZE).decode(FORMAT).strip()
-            
-            filesize = conn.recv(SIZE).decode(FORMAT).strip()
-            
-            global COUNTER
-            
-            send_data = f"File size received {filesize} bytes.\n"
-            conn.send(send_data.encode(FORMAT))
-            print(f"Receiving filesize of {filesize} bytes")
-            
-            # checks if the file has already been uploaded
-            if (filename in uploaded):
-                # if it's been uploaded, send a 1 to client to let it know
-                send_data = "File received successfully. 1\n"
-                conn.send(send_data.encode(FORMAT))
-                
-                # receive if user wants to overwrite it (Y/N)
-                ans = conn.recv(SIZE).decode(FORMAT)
-                
-                # if user wants to overwrite, upload file again
-                if (ans == "Y"):
-                    # receive data, copy it
-                    new_filename = "TS" + ("%03d" % COUNTER)
-                    received_data = 0
-                    with open(new_filename, "wb") as fo:
-                        print("Receiving file...")
-                        
-                        while received_data < int(filesize):
-                            print("Receiving...")
-                            data = conn.recv(SIZE)
-                            if not data:
-                                break
-                            print(f"Received {len(data)} bytes")
-                            fo.write(data)
-                            received_data += len(data)
-                            print("Wrote to file")
-                        fo.close()
-                    COUNTER += 1
-                    
-                    # tell yourself that you got it w/ location
-                    print(f"File {filename} received from {addr}")
-                    
-                    # send final confirmation
-                    send_data = f"OK@File {filename} overwritten.\n"
-                    conn.send(send_data.encode(FORMAT))
-                
-                # if user doesn't want to overwrite, end
-                else:
-                    send_data = f"OK@File {filename} not overwritten.\n"
-                    conn.send(send_data.encode(FORMAT))
-            
-            # if file not already uploaded, upload the filee
-            else:
-                
-                # send confirmation, client_1 receives it
-                send_data = "File received successfully.\n"
-                conn.send(send_data.encode(FORMAT))
-                
-                # receive data, copy it
-                new_filename = "TS" + ("%03d" % COUNTER)
-                received_data = 0
-                with open(new_filename, "wb") as fo:
-                    print("Receiving file...")
-                    
-                    while received_data < int(filesize):
-                        print("Receiving...")
-                        data = conn.recv(SIZE)
-                        if not data:
-                            break
-                        print(f"Received {len(data)} bytes")
-                        fo.write(data)
-                        received_data += len(data)
-                        print("Wrote to file")
-                    fo.close()
-                COUNTER += 1
-                
-                # add uploaded file to the uploaded list so we know we have it
-                uploaded.append(filename)
-                print(uploaded)
-                
-                # tell yourself that you got it w/ location
-                print(f"File {filename} received from {addr}")
-                
-                # send final confirmation
-                send_data = f"OK@File {filename} uploaded as {new_filename} successfully.\n"
-                conn.send(send_data.encode(FORMAT))
         
-        # TO DO
-        elif cmd == "DOWNLOAD":
-            # send to client_1 that you're ready
-            send_data += "Ready to receive the file.\n"
-            conn.send(send_data.encode(FORMAT))
-            print(uploaded)
-            # get filename
-            filename = conn.recv(SIZE).decode(FORMAT).strip()
+        data = input("> ") 
+        data = data.split(" ")
+        cmd = data[0]
+
+        if cmd == "TASK":
+            client.send(cmd.encode(FORMAT))
+
+        elif cmd == "LOGOUT":
+            client.send(cmd.encode(FORMAT))
+            break
+        
+        elif cmd == "UPLOAD":
+            # send to server that cmd is UPLOAD so it chooses UPLOAD of its functions 
+            client.send(cmd.encode(FORMAT))
             
-            if (filename in uploaded):
+            # receive "ready to receive" to continue
+            client.recv(SIZE).decode(FORMAT)
+            
+            # send filename to be saved in the server 
+            filename = input("Enter the filename to upload: ")
+            client.send(filename.encode(FORMAT))
+            
+
+            fsize = os.path.getsize(filename)
+            print(f"File size: {fsize} bytes")
+            client.send(str(fsize).encode(FORMAT))
+            
+            client.recv(SIZE).decode(FORMAT) # size confirm
+            print("size confirmation received")
+
+            # receive "file received" or "file received 1" to see if it's already uploaded
+            rec = client.recv(SIZE).decode(FORMAT)
+            print("got confirm")
+
+            # if file is already uploaded
+            if ("1" in rec):
+                # ask if user wants to overwrite it, send it to server
+                ans = input(f"File {filename} already exists. Would you like to overwrite it? (Y/N)\n> ")
+                client.send(ans.encode(FORMAT))
                 
-                continue
-                ### IMPLEMENT SENDING FILE TO CLIENT
+                # if their answer was Y, send file contents
+                if (ans == "Y"):
+                    with open(filename,"rb") as fo:
+                        print ("Uploading file...")
+                        while True:
+                            print("Uploading...")
+                            data = fo.read(SIZE)
+                            if not data:
+                                client.send(data)
+                                break
+                            print(f"Read {len(data)} bytes")
+                            client.sendall(data)
+                            print("Sent a chunk")
+                        fo.close()
                 
+                # if their answer was N, do nothing
+            
+            # if file is not already uploaded, send file contents
             else:
-                send_data = "File received successfully. 1\n"
-                conn.send(send_data.encode(FORMAT))
-                
-                send_data = f"OK@File {filename} is unable to be downloaded: does not exist.\n"
-                conn.send(send_data.encode(FORMAT))
+                with open(filename,"rb") as fo:
+                    print ("Uploading file...")
+                    while True:
+                        print("Uploading...")
+                        data = fo.read(SIZE)
+                        if not data:
+                            client.send(data)
+                            break
+                        print(f"Read {len(data)} bytes")
+                        client.sendall(data)
+                        print("Sent a chunk")
+                    fo.close()
+            #client.send("EOF".encode(FORMAT))  ## indicate end of file
+            print(f"File {filename} uploaded to the server.")
+    
+    # TO DO
+        elif cmd == "DOWNLOAD":
+            # send to server that cmd is DOWNLOAD so it chooses DOWNLOAD of its functions 
+            client.send(cmd.encode(FORMAT))
+            
+            # receive "ready to send" to continue
+            client.recv(SIZE).decode(FORMAT)
+            
+            # send filename to be sent from the server
+            filename = input("Enter the filename to download: ")
+            client.send(filename.encode(FORMAT))
+            
+            if ("1" not in client.recv(SIZE).decode(FORMAT)):
+                continue
+                ### IMPLEMENT RECEIVING FILE FROM SERVER
         
         elif cmd == "DELETE":
-            # send to client_1 that you're ready
-            send_data += "Ready to receive the file.\n"
-            conn.send(send_data.encode(FORMAT))
+            # send to server that cmd is DELETE so it chooses DELETE of its functions 
+            client.send(cmd.encode(FORMAT))
             
-            # get filename
-            filename = conn.recv(SIZE).decode(FORMAT).strip()
+            # receive "ready to receive" to continue
+            client.recv(SIZE).decode(FORMAT)
             
-            # if file exists in server, delete it
-            if (filename in uploaded):
-                uploaded.remove(filename)
-                
-                send_data = f"OK@File {filename} successfully deleted.\n"
-                conn.send(send_data.encode(FORMAT))
-            
-            # if file does not exist in server, do nothing and say it doesn't exist
-            else:
-                send_data = f"OK@File {filename} unable to be deleted: does not exist.\n"
-                conn.send(send_data.encode(FORMAT))
-            
+            # ask user what to delete
+            filename = input("Enter the filename to delete: ")
+            client.send(filename.encode(FORMAT))
+        
         # TO DO
         elif cmd == "DIR":
             continue
@@ -189,68 +134,15 @@ def handle_client (conn,addr):
         elif cmd == "SUBFOLDER":
             continue
         
-        elif cmd == "ANALYZE":
-            send_data = f"OK@Please only use the ANALYZE function using analysis.py.\n"
-            conn.send(send_data.encode(FORMAT))
-            
-        elif cmd == "AN1ANALYZE":
-            send_data += "Analysis request received"
-            conn.send(send_data.encode(FORMAT))
-            
-            if (os.path.isfile("connection_time.txt") == True):
-                with open("connection_time.txt","r") as fo:
-                    data = fo.read(SIZE)
-                    if not data:
-                        send_data = "File is empty. 1"
-                        conn.send(send_data.encode(FORMAT))
-                        send_data = "OK@Server connection time does not exist."
-                        conn.send(send_data.encode(FORMAT))
-                    else:
-                        send_data = "Analyzing connection time..."
-                        conn.send(send_data.encode(FORMAT))
-                        # continues past this if/else!!!, will analyze other things. Leave this part alone, make more if/thens below
-            else:
-                send_data = "File does not exist. 1"
-                conn.send(send_data.encode(FORMAT))
-                
-                send_data = "OK@Server connection time does not exist."
-                conn.send(send_data.encode(FORMAT))
-            # confirm that files are there, analysis.py will save them to dictionary
-        
         else:
-            if "AN1" not in cmd:
-                send_data = f"OK@There is no option for {cmd}. Please try again or type TASK for help.\n"
-                conn.send(send_data.encode(FORMAT))
-            else:
-                send_data = f"OK@There is no option for {cmd[3:]}. Please type ANALYZE to analyze performance data.\n"
-                conn.send(send_data.encode(FORMAT))
+            client.send(cmd.encode(FORMAT))
+      
 
 
-    print(f"{addr} disconnected")
-    conn.close()
+    print("Disconnected from the server.")
+    client.close() ## close the connection
 
-def write_conn_time(conn_time):
-    with open("connection_time.txt", "w") as fo:
-        fo.write(str(conn_time))
-        fo.close()
-
-def main():
-    print("Starting the server")
-    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM) ## used IPV4 and TCP connection
-    server.bind(ADDR) # bind the address
-    server.listen() ## start listening
-    print(f"server is listening on {IP}: {PORT}")
-    os.system("start cmd /k python analysis.py")
-    while True:
-        conn, addr = server.accept() ### accept a connection from a client
-        t1 = time()
-        thread = threading.Thread(target = handle_client, args = (conn, addr)) ## assigning a thread for each client
-        thread.start()
-        t2 = time()
-        conn_time = t2 - t1
-        print(f"Connection time: {conn_time}")
-        write_conn_time(conn_time)
-
+# DO MAIN 
 
 if __name__ == "__main__":
     main()
